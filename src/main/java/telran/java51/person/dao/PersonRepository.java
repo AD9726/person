8deaf88e6e1d0468f69010477e0356e099283b19
Dani <dani.aguvaev.97@mail.ru>
2024-03-14 02:17:37 +0200

    Complete child, employee and person

--- src/main/java/telran/java51/person/dao/PersonRepository.java
+++ src/main/java/telran/java51/person/dao/PersonRepository.java
@@ -12,10 +12,8 @@ import telran.java51.person.dto.CityPopulationDto;
 import telran.java51.person.model.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Integer> {
-	@Query("select p from Person p where p.name=?1")
 	Stream<Person> findByNameIgnoreCase(String name);
 	
-	@Query("select p from Person p where p.address.city=:cityName")
 	Stream<Person> findByAddressCityIgnoreCase(@Param("cityName") String city);
 	
 	Stream<Person> findByBirthDateBetween(LocalDate from, LocalDate to);
